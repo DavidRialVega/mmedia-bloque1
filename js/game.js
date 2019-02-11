90fc067a7def679ae4ed106292cfcc6c3fca7136
juanmaverano <juanmita10@hotmail.com>
2019-02-11 18:20:00 +0100

    pagina 10 ultimo PDF

--- js/game.js
+++ js/game.js
@@ -138,7 +138,10 @@ var levels={
             //segundo nivel
             foreground:'desert-foreground',
             background:'clouds-background',
-            entities:[]
+            entities:[
+                {type:"block", name:"wood", x:520,y:375,angle:90},
+                {type:"villain", name:"burger",x:520,y:200,calories:90},
+            ]
         }
     ],
 
@@ -253,5 +256,81 @@ var mouse = {
     mouseuphandler: function(ev){
         mouse.down = false;
         mouse.dragging = false;
+    }
+}
+
+var entities = {
+    definitions : {
+      "glass" : {
+        fullHealth : 100,
+        density : 2.4,
+        friction : 0.4,
+        restitution : 0.15,
+      },
+      "wood" : {
+        fullHealth : 500,
+        density :0.7,
+        friction : 0.4,
+        restitution : 0.4,
+      },    
+      "dirt" : {
+        density : 3.0,
+        friction : 1.5,
+        restitution : 0.2,
+      },
+      "burger" : {
+        shape : "circle",
+        fullHealth : 40,
+        radius : 25,
+        density : 1,
+        friction : 0.5,
+        restitution : 0.4,
+      },
+      "sodacan" : {
+        shape : "rectangle",
+        fullHealth : 80,
+        width : 40,
+        height : 60,
+        density : 1,
+        friction : 0.5,
+        restitution : 0.7,
+      },
+      "fries" : {
+        shape : "rectangle",
+        fullHealth : 50,
+        width : 40,
+        height : 50,
+        density : 1,
+        friction : 0.5,
+        restitution : 0.6,
+      },
+      "apple" : {
+        shape : "circle",
+        radius : 25,
+        density : 1.5,
+        friction : 0.5,
+        restitution : 0.4,
+      },
+      "orange" : {
+        shape : "circle",
+        radius : 25,
+        density : 1.5,
+        friction : 0.5,
+        restitution : 0.4,
+      },
+      "strawberry" : {
+        shape : "circle",
+        radius : 15,
+        density : 2.0,
+        friction : 0.5,
+        restitution : 0.4,
+      }
+    },
+
+    create:function(enttity){
+
+    },
+    draw:function(enttity,position,angle){
+
     }
 }
